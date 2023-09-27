45dbc61bba6cc038a03265f7339e2a71552e37d4
dantheman <danogbo0@gmail.com>
2023-09-27 20:41:12 +0100

    model adjust

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -8,7 +8,7 @@
 <meta name="author" content="">
 <meta name="keywords" content="MediaCenter, Template, eCommerce">
 <meta name="robots" content="all">
-<title>Flipmart premium HTML5 & CSS3 Template</title>
+<title>Flipmart</title>
 
 
 <!-- Bootstrap Core CSS -->
